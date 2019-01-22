930f196614109439826daf8af329ba0ff5275341
jeremysher13 <jeremy.sher@yahoo.com>
2019-01-22 15:26:13 -0500

    Instantiated motors and set Drive() as default command

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -8,6 +8,8 @@ import frc.robot.driver.*;
 import frc.robot.subsystems.*;
 import frc.robot.util.*;
 import frc.robot.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
@@ -16,17 +18,24 @@ public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  private CANSparkMax leftMotor;
+  private CANSparkMax rightMotor;
+
   public DriveTrain() {
-    
+    leftMotor = new CANSparkMax(RobotMap.LEFT_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightMotor = new CANSparkMax(RobotMap.RIGHT_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+
   }
 
   public void tankDrive(double leftSpeed, double rightSpeed){
-   // RobotMap.LEFT_TALON_0.
+    //may need to adjust speed values
+    leftMotor.set(leftSpeed);
+    rightMotor.set(rightSpeed);
   }
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new Drive());
   }
 }
