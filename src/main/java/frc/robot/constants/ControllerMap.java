93db599d3a61ba2bcee1347e236a300ebcc1edfb
jeremysher13 <jeremy.sher@yahoo.com>
2019-01-29 16:04:45 -0500

    made controller constants final

--- src/main/java/frc/robot/constants/ControllerMap.java
+++ src/main/java/frc/robot/constants/ControllerMap.java
@@ -9,24 +9,24 @@ import frc.robot.*;
 
 public class ControllerMap {
 
-    public static int DRIVER_PORT = 0;
-    public static int WEAPONS_PORT = 1;
-    public static int A_BUTTON_PORT = 1;
-    public static int B_BUTTON_PORT = 2;
-    public static int X_BUTTON_PORT = 0;
-    public static int Y_BUTTON_PORT = 3;
+    public static final int DRIVER_PORT = 0;
+    public static final int WEAPONS_PORT = 1;
+    public static final int A_BUTTON_PORT = 1;
+    public static final int B_BUTTON_PORT = 2;
+    public static final int X_BUTTON_PORT = 0;
+    public static final int Y_BUTTON_PORT = 3;
     public static int DPAD_UP_PORT;
     public static int DPAD_DOWN_PORT;
     public static int DPAD_LEFT_PORT;
     public static int DPAD_RIGHT_PORT;
-    public static int LB_PORT = 4;
-    public static int LT_PORT = 6;
-    public static int RB_PORT = 5;
-    public static int RT_PORT = 7;
-    public static int LJ_PORT = 10;
-    public static int RJ_PORT = 11;
-    public static int START_PORT = 9;
-    public static int BACK_PORT = 8;
+    public static final int LB_PORT = 4;
+    public static final int LT_PORT = 6;
+    public static final int RB_PORT = 5;
+    public static final int RT_PORT = 7;
+    public static final int LJ_PORT = 10;
+    public static final int RJ_PORT = 11;
+    public static final int START_PORT = 9;
+    public static final int BACK_PORT = 8;
     public static int HOME_PORT;
 
 }
