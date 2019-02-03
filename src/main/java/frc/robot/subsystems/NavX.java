f3376c02760b6ddd0bb55cdf40a92d1f3bd81309
Ryan Bohinski <rjbohinski@gmail.com>
2019-02-03 17:34:40 -0500

    Fix package error and use correct connect function.

--- src/main/java/frc/robot/subsystems/NavX.java
+++ src/main/java/frc/robot/subsystems/NavX.java
@@ -1,18 +1,10 @@
-package main.java.frc.robot.subsystems;
-
-import frc.robot.commands.*;
-import frc.robot.constants.*;
-import frc.robot.driver.*;
-import frc.robot.util.*;
-import frc.robot.*;
+package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class NavX extends Subsystem {
     public static AHRS navx;
@@ -25,7 +17,7 @@ public class NavX extends Subsystem {
     //ports
     public static synchronized boolean connectUSB() throws RuntimeException {
 
-        if (navx == null || !navx.isConnected()) { 
+        if (navx == null || !navx.isConnected()) {
             navx = new AHRS(SerialPort.Port.kUSB);
             System.out.println("NavX Connected: " + navx.isConnected());
         }
@@ -77,12 +69,11 @@ public class NavX extends Subsystem {
     @Override
     public void periodic() {
         super.periodic();
-        NavX.connect();
+        NavX.connectUSB();
     }
 
-    //call method
     public static synchronized AHRS getNavx() throws NullPointerException {
-        if (NavX.connect()) {
+        if (NavX.connectUSB()) {
             return navx;
         } else {
             throw new NullPointerException("Failed to connect to the NavX!");
