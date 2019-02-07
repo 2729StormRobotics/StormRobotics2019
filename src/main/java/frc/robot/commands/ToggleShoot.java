5d19eda6ea513b7b1e24b92f5b29daa9f11e2926
jeremysher13 <jeremy.sher@yahoo.com>
2019-02-07 15:23:45 -0500

    changed pneumatic Commands to InstantCommands
    Co-Authored-By: Pranay M. <pmusalimadugu@gmail.com>

--- src/main/java/frc/robot/commands/ToggleShoot.java
+++ src/main/java/frc/robot/commands/ToggleShoot.java
@@ -5,12 +5,13 @@ import frc.robot.driver.*;
 import frc.robot.subsystems.*;
 import frc.robot.util.*;
 import frc.robot.*;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
-public class ToggleShoot extends Command {
+public class ToggleShoot extends InstantCommand {
 
     public ToggleShoot() {
-        requires(Robot.pneumatics); // airsystem is an instance of our Airsystem subsystem
+        super();
+        requires(Robot.pneumatics);
     }
 
     @Override
@@ -18,29 +19,4 @@ public class ToggleShoot extends Command {
         Robot.pneumatics.shootCargo();
     }
 
-    /*
-     * execute() - In our execute method we call a tankDrive method we have created in our subsystem. This method takes two speeds as a parameter which we get from methods in the OI class.
-     * These methods abstract the joystick objects so that if we want to change how we get the speed later we can do so without modifying our commands
-     * (for example, if we want the joysticks to be less sensitive, we can multiply them by .5 in the getLeftSpeed method and leave our command the same).
-     */
-    @Override
-    protected void execute() {
-    }
-
-    /*
-     * isFinished - Our isFinished method always returns true, meaning this command completes during initialization
-     */
-    @Override
-    protected boolean isFinished() {
-        return true;
-    }
-
-    @Override
-    protected void end() {
-    }
-
-    @Override
-    protected void interrupted() {
-        end();
-    }
 }
