2aead084e7ba0cc9c8b5ce5a0447be0605d7e16f
gregoryhazen <gregoryhazen@gmail.com>
2019-03-12 16:26:49 -0400

    added point turn constants

--- src/main/java/frc/robot/constants/PIDMap.java
+++ src/main/java/frc/robot/constants/PIDMap.java
@@ -6,4 +6,11 @@ public class PIDMap {
     public static final double DRIVE_DISTANCE_D = 0.0;
     public static final double DRIVE_DISTANCE_PERIOD = 0.050;
     public static final double DRIVE_DISTANCE_TOLERANCE = 0.5;
+
+    public static final double POINT_TURN_P = 0.1;
+    public static final double POINT_TURN_I = 0.0;
+    public static final double POINT_TURN_D = 0.0;
+    public static final double POINT_TURN_PERIOD = 0.050;
+    public static final double POINT_TURN_TOLERANCE = 0.5;
+
 }
